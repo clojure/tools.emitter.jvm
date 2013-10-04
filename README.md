78055dcded390e137063b7d7ad7a2f8473da97c1
Alex Miller <alex@puredanger.com>
2013-10-04 11:41:32 -0500

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# tools.emitter.jvm
+
+A JVM bytecode generator for ASTs compatible with tools.analyzer(.jvm).
+
+## Releases and Dependency Information
+
+
+
+## License
+
+Copyright © 2013 Nicola Mometto, Rich Hickey & contributors.
+
+Distributed under the Eclipse Public License, the same as Clojure.
+
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tools.emitter.jvm "0.0.1-SNAPSHOT"
+  :description "A JVM bytecode generator for ASTs compatible with tools.analyzer(.jvm)."
+  :url "https://github.com/clojure/tools.emitter.jvm"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
