e4a4104699da35c030ed95e96c35ba7101e9c323
Bronsa <brobronsa@gmail.com>
2013-10-22 03:17:42 +0200

    special handling for do evaluation

--- src/main/clojure/clojure/tools/emitter/jvm.clj
+++ src/main/clojure/clojure/tools/emitter/jvm.clj
@@ -9,14 +9,23 @@
 (ns clojure.tools.emitter.jvm
   (:refer-clojure :exclude [eval])
   (:require [clojure.tools.analyzer.jvm :as a]
+            [clojure.tools.analyzer :refer [macroexpand-1]]
             [clojure.tools.emitter.jvm.emit :as e])
   (:import clojure.lang.IFn))
 
 (defn eval
   ([form] (eval form false))
   ([form debug?]
-     (let [r (e/emit (a/analyze `(^:once fn* [] ~form) (a/empty-env))
-                     {:debug? debug?
-                      :class-loader (clojure.lang.RT/makeClassLoader)})
-           {:keys [class]} (meta r)]
-       (.invoke ^IFn (.newInstance ^Class class)))))
+     (let [mform (binding [macroexpand-1 a/macroexpand-1]
+                   (macroexpand form))]
+       (if (and (seq? mform) (= 'do (first mform)))
+         (let [statements (butlast (rest mform))
+               ret (last mform)]
+           (doseq [expr statements]
+             (eval expr debug?))
+           (eval ret debug?))
+         (let [r (e/emit (a/analyze `(^:once fn* [] ~form) (a/empty-env))
+                         {:debug? debug?
+                          :class-loader (clojure.lang.RT/makeClassLoader)})
+               {:keys [class]} (meta r)]
+           (.invoke ^IFn (.newInstance ^Class class)))))))
