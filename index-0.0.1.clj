8e570623ec58215e381ac4cbb0ee473ce527d06d
Tom Faulhaber <git_net@infolace.com>
2014-01-27 18:22:47 -0800

    Autodoc commit for master/6d31ba8d

--- index-0.0.1.clj
+++ index-0.0.1.clj
@@ -1,6 +1,6 @@
 {:namespaces
  ({:source-url
-   "https://github.com/clojure/tools.emitter.jvm/blob/8a5de2b884eaa5ce9b3afc44a6d50a147b528bad/src/main/clojure/clojure/tools/emitter/jvm.clj",
+   "https://github.com/clojure/tools.emitter.jvm/blob/61a7375717c6eb64a858d6b7e56c36ed9a246035/src/main/clojure/clojure/tools/emitter/jvm.clj",
    :wiki-url
    "http://clojure.github.com/tools.emitter.jvm/clojure.tools.emitter.jvm-api.html",
    :name "clojure.tools.emitter.jvm",
