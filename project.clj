73de111afc6a9570189ff46a253b0669c4f3871a
Nicola Mometto <brobronsa@gmail.com>
2014-09-17 01:58:33 +0200

    bump dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
                  [org.clojure/tools.analyzer.jvm "0.5.7-SNAPSHOT"]
                  [org.clojure/tools.reader "0.8.8"]
                  [org.ow2.asm/asm-all "4.2"]])
