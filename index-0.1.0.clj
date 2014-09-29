ccc827324c33488bff7f660d3444f48d3be8b128
Tom Faulhaber <git_net@infolace.com>
2014-09-29 10:27:14 -0700

    Autodoc commit for master/c5892549

--- index-0.1.0.clj
+++ index-0.1.0.clj
@@ -1,6 +1,6 @@
 {:namespaces
  ({:source-url
-   "https://github.com/clojure/tools.emitter.jvm/blob/9dd866390082138e27973e030d73cb7ff7420057/src/main/clojure/clojure/tools/emitter/jvm.clj",
+   "https://github.com/clojure/tools.emitter.jvm/blob/71f1de2c3592e2ea0fd78969f176686831621407/src/main/clojure/clojure/tools/emitter/jvm.clj",
    :wiki-url
    "http://clojure.github.com/tools.emitter.jvm/clojure.tools.emitter.jvm-api.html",
    :name "clojure.tools.emitter.jvm",
@@ -13,6 +13,51 @@
    :doc nil}),
  :vars
  ({:arglists
+   ([form]
+    [form
+     {:keys [debug? emit-opts class-loader analyze-opts],
+      :or
+      {debug? false,
+       emit-opts {},
+       analyze-opts a/default-passes-opts,
+       class-loader (clojure.lang.RT/makeClassLoader)},
+      :as options}]),
+   :name "eval",
+   :namespace "clojure.tools.emitter.jvm",
+   :source-url
+   "https://github.com/clojure/tools.emitter.jvm/blob/71f1de2c3592e2ea0fd78969f176686831621407/src/main/clojure/clojure/tools/emitter/jvm.clj#L54",
+   :raw-source-url
+   "https://github.com/clojure/tools.emitter.jvm/raw/71f1de2c3592e2ea0fd78969f176686831621407/src/main/clojure/clojure/tools/emitter/jvm.clj",
+   :wiki-url
+   "http://clojure.github.com/tools.emitter.jvm//clojure.tools.emitter.jvm-api.html#clojure.tools.emitter.jvm/eval",
+   :doc
+   "(eval form)\n (eval form eval-options-map)\n\nForm is a read Clojure s expression represented as a list.\nEval-options-map is a map, defaulting to the empty map, the\nfollowing values of which are significant. Returns the result of\nevaling the input expression.\n\nOptions\n-----------\n:debug? :- (Option Bool)\n  Enables or disables printing in eval. Used as the default value for\n  printing in the emitter.\n\n:emit-opts :- (Option emit-options-map)\n  An options map which will be merged with the default options\n  provided to emit. Keys in this map take precidence over the default\n  values provided to emit. The keys which are significant in this map\n  are documented in the t.e.jvm.emit/emit docstring.\n\n:analyze-opts :- (Option analyze-options-map)\n  An options map that will be passed to the analyzer. The keys which\n  are significant in this map are documented in the t.a.jvm/analyze\n  docstring.",
+   :var-type "function",
+   :line 54,
+   :file "src/main/clojure/clojure/tools/emitter/jvm.clj"}
+  {:arglists
+   ([res]
+    [res
+     {:keys [debug? eval-opts class-loader],
+      :or
+      {debug? false,
+       eval-opts {},
+       class-loader (clojure.lang.RT/makeClassLoader)},
+      :as options}]),
+   :name "load",
+   :namespace "clojure.tools.emitter.jvm",
+   :source-url
+   "https://github.com/clojure/tools.emitter.jvm/blob/71f1de2c3592e2ea0fd78969f176686831621407/src/main/clojure/clojure/tools/emitter/jvm.clj#L107",
+   :raw-source-url
+   "https://github.com/clojure/tools.emitter.jvm/raw/71f1de2c3592e2ea0fd78969f176686831621407/src/main/clojure/clojure/tools/emitter/jvm.clj",
+   :wiki-url
+   "http://clojure.github.com/tools.emitter.jvm//clojure.tools.emitter.jvm-api.html#clojure.tools.emitter.jvm/load",
+   :doc
+   "(load resource)\n (load resource load-options-map)\n\nResource is a string identifier for a Clojure resource on the\nclasspath. Load-options is a a map, defalting to the empty map, in\nwhich the following keys are meaningful. Returns nil.\n\nOptions\n-----------\n:debug? :- (Option Bool)\n  Enables or disables printing in eval. Used as the default value for\n  printing in the emitter.\n\n:eval-opts  :- (Option eval-options-map)\n  An options map which will be merged with the default options\n  provided to eval. Keys set in this map take precidence over the\n  default values supplied to eval. The keys which are significant in\n  this map are documented in the t.e.jvm/eval docstring.\n\n:class-loader :- (Option ClassLoader)\n  An optional classloader into which compiled functions will be\n  injected. If not provided, a new Clojure classloader will be\n  used. If a class loader is provided here, one need not be provided\n  in eval-opts.",
+   :var-type "function",
+   :line 107,
+   :file "src/main/clojure/clojure/tools/emitter/jvm.clj"}
+  {:arglists
    ([ast] [{:keys [env o-tag tag op type unchecked?], :as ast} frame]),
    :name "emit",
    :namespace "clojure.tools.emitter.jvm.emit",
