82d8024e5c7de90ba32b5ad8038e2a87a2cb6166
Reid 'arrdem' McKenzie <reid@factual.com>
2015-07-21 17:18:36 -0700

    More test cases

--- src/test/clojure/clojure/tools/emitter/jvm/core_test.clj
+++ src/test/clojure/clojure/tools/emitter/jvm/core_test.clj
@@ -13,7 +13,28 @@
   (is (= [1 2] (e/eval '(:foo {:foo [1 2]}))))
   (is (= 3 (e/eval '(do (def a 3) 3))))
   (is (= 6 (e/eval '(do (def g (fn [x] (* x 3))) (g 2)))))
-  (is (= 3 (e/eval '(first (remove #(not= 3 %) (filter odd? (map inc (range 10)))))))))
+  (is (= 3 (e/eval '(first (remove #(not= 3 %) (filter odd? (map inc (range 10))))))))
+  (is (thrown? Exception (e/eval '(throw (Exception. "Foo!")))))
+  (is (= 'foo (e/eval '(quote foo))))
+  (is (= #'conj (e/eval '(var conj))))
+  (is (= {:a :b}
+         (e/eval '(let [x :b] {:a x}))
+         (e/eval '{:a :b})))
+  (is (= #{:a :b}
+         (e/eval '#{:a :b})))
+  (is (= [:a :b]
+         (e/eval '[:a :b])))
+  (is (= {:a :b}
+         (meta (e/eval '(with-meta [:c :d] {:a :b})))))
+  (is (= :catch
+         (e/eval '(try (throw (Exception. "Foo!"))
+                       (catch Exception e :catch)))))
+  (is (= :finally
+         (e/eval '(let [x (atom 0)]
+                    (try (throw (Exception. "Foo!"))
+                         (catch Exception e :catch)
+                         (finally (reset! x :finally)))
+                    @x)))))
 
 
 
